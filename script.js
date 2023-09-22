2bdbdcd775a355a77b9f508dc529064e9642943f
parsa_khoshnama <parsa2001khoshnama@gmail.com>
2023-09-22 23:16:24 +0330

    commit

--- script.js
+++ script.js
@@ -7,25 +7,37 @@ let date=new Date()
 let hours=date.getHours()
 let minutes=date.getMinutes()
 let seconds=date.getSeconds()
+
+
+
 let degOfSeconds
 let degOfMinutes
 let degOfHours
 
+let daysCount=0
+
 rotateOfHands()
+
+
 setTimeout(function(){
-secondsElemnt.style.transition='transform 1s'
-minutesElement.style.transition='transform 1s'
-hoursElement.style.transition='transform 1s'
+secondsElemnt.style.transition='transform 0.000001ms'
+minutesElement.style.transition='transform 0.000001ms'
+hoursElement.style.transition='transform 0.000001ms'
 },0.001)
-/*
+
 setInterval(function(){
     delete date
     date=new Date()
     hours=date.getHours()
     minutes=date.getMinutes()
     seconds=date.getSeconds()
+    if(hours==0){
+        daysCount++
+        hours=24*daysCount
+    }
     rotateOfHands()
-},1000)*/
+},1000)
+
 
 
 
@@ -33,11 +45,10 @@ setInterval(function(){
 
 
 function rotateOfHands(){
-    degOfSeconds=(seconds*6)
-    degOfMinutes=(minutes*6)+(seconds*0.1)
-    if(hours>=12)
-        hours=hours-12
-    degOfHours=(hours*30)+(minutes/2)+(seconds/120)
+   seconds=(hours*3600)+(minutes*60)+seconds
+    degOfSeconds=seconds*6
+    degOfMinutes=seconds/10
+    degOfHours=seconds/120
     secondsElemnt.style.transform='translate(-50%,-50%) rotate('+(degOfSeconds)+'deg)'
     minutesElement.style.transform='translate(-50%,-50%) rotate('+(degOfMinutes)+'deg)'
     hoursElement.style.transform='translate(-50%,-50%) rotate('+(degOfHours) +'deg)'
